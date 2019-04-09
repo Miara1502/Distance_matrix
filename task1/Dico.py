79f03f9e31c11da5e08162d8b7da8860f5c91e3d
miara1502 <miara.rakotomavo@gmail.com>
2019-04-09 05:08:04 -0700

    Dico.py

---
+++ task1/Dico.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+
+
+import pandas as pd
+import numpy as np
+import scipy
+import multiprocessing
+import math
+
+dt = pd.read_csv('1000genes', sep = '\t')
+#dt = pd.read_csv('test_GE_Dist_Miara.tab', sep = '\t')
+
+#Distance MATRIX
+from scipy.spatial import distance_matrix
+ndarray = scipy.spatial.distance.pdist(dt)
+
+matrix_uni = scipy.spatial.distance.squareform(ndarray)
+matrix = pd.DataFrame(matrix_uni)
+
+for i in range(len(matrix)) :
+    matrix= matrix.rename(index= {i:dt.index[i]})
+    matrix= matrix.rename(columns= {i:dt.index[i]});
+
+#Création dictionnaire :
+def dico_matrice(matrice):
+    dico = {}
+    for i in range(len(matrice)):
+        #dico[dt.index[i]] =  matrice[dt.index[i]].sort_values()[1:10].index
+        dico[dt.index[i]] =   matrice[dt.index[i]].sort_values()[1:11]
+    return dico;
+
+from multiprocessing import Pool
+with Pool(5) as p :
+    print(p.map(dico_matrice,[matrix]))
+
+#DICO = dico_matrice(matrix)
+
+multiprocessing.cpu_count()
